86787ab75987dd8738db450371bdc5719faf0d5f
Dmytro Lisitsyn <dmytro.l@bizzabo.com>
2024-05-07 21:00:23 +0300

    Make a draft of ios dev configuration

---
+++ lua/settings.lua
@@ -0,0 +1,23 @@
+-- Use `:h <option>` to figure out the meaning if needed
+
+vim.g.mapleader = " "
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.swapfile = false
