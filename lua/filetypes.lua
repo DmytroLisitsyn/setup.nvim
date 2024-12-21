b82044b43f023c367bc58de8eb5862b47e80fcf0
Dmytro Lisitsyn <dmytro.l@bizzabo.com>
2024-12-21 19:19:45 +0200

    Remove unused plugins

---
+++ lua/filetypes.lua
@@ -0,0 +1,8 @@
+-- Custom language to file raltions
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = { 'Appfile', 'Fastfile', 'Matchfile', 'Pluginfile' },
+  callback = function()
+    vim.opt.filetype = 'ruby'
+  end,
+})
